346386b54141ffdc0b688106b2a05ca4729e7568
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-05-28 09:54:05 +0300

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 
 @Component({
@@ -9,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor() { }
-  ngOnInit(): void {}
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) this.basketService.getBasket(basketId);
+  }
 }
