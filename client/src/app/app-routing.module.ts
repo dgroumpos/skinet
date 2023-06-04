fd297842b1d8d1680cb1df3091a0b5615db2b9ec
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-06-04 21:15:18 +0300

    End of section 20

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,23 +7,41 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)},
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  { path: 'test-error', component: TestErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((m) => m.BasketModule),
+  },
   {
     path: 'checkout',
     canActivate: [authGuard],
-    loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((m) => m.CheckoutModule),
   },
-  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {
+    path: 'orders',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((m) => m.AccountModule),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
